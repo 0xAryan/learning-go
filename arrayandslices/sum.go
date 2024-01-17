df0335cadbd5fd7f8bb6cba7be5150f079fd9c4d
Aryan <Oxaryan@protonmail.com>
2024-01-17 16:07:52 +0530

    Completed chapter 5 arrays&slices

--- arrayandslices/sum.go
+++ arrayandslices/sum.go
@@ -1,5 +1,7 @@
 package arrayandslices
 
+
+
 func Sum(numbers []int) int {
 
 	var sum int = 0
@@ -7,4 +9,31 @@ func Sum(numbers []int) int {
 		sum = sum + number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		}else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+
+	}
+
+	return sums
 }
